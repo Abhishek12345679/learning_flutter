79cefb1084c15cd5a37d807c1441c03ec7a19afa
Abhishek12345679 <sah755146@gmail.com>
2023-03-13 23:48:43 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  // final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Abhishek"),
+      ),
+      body: Center(
+          child: ListView(children: const [
+        Text("Abhishek"),
+        Text("Abhishek"),
+        Text("Abhishek"),
+        Text("Abhishek"),
+        Text("Abhishek"),
+        Text("Abhishek"),
+      ])),
+    );
+  }
+}
